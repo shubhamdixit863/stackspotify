0eeefd47cd410c634e39ca6b7cacafc26a8ed024
shubham dixit <shubham.dixit@mobcoder.com>
2021-03-08 17:53:40 +0530

    updated

--- src/app/artist-discography/artist-discography.component.ts
+++ src/app/artist-discography/artist-discography.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as albumData from '../data/SearchResultsAlbums.json';
+import * as artistData from '../data/SearchResultsArtist.json';
 
 @Component({
   selector: 'app-artist-discography',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-discography.component.css']
 })
 export class ArtistDiscographyComponent implements OnInit {
+  album:any;
+  artist:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.album=albumData.albums.items;
+    this.artist=(artistData as any).default;
   }
 
 }
